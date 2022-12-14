3afd1012bec765bcd4351f478099a22c3af8ebfe
tringuyen252001@gmail.com <tringuyen252001@gmail.com>
2022-12-14 21:27:24 +0700

    update readme

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,6 +1,5 @@
 import {
    Button,
-   Container,
    Grid,
    IconButton,
    InputAdornment,
@@ -12,6 +11,33 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+   return (
+      <Typography
+         variant="body2"
+         color="text.secondary"
+         align="center"
+         {...props}
+      >
+         {"Copyright © "}
+         <Link color="inherit" href="">
+            Englist Hub
+         </Link>{" "}
+         {new Date().getFullYear()}
+         {"."}
+      </Typography>
+   );
+}
+const theme = createTheme();
 
 function Login() {
    const navigate = useNavigate();
@@ -22,16 +48,10 @@ function Login() {
       role: "",
       showPass: false,
    });
-   // const [, setUser] = useContext(UserContext);
    const handleSubmit = (e) => {
       e.preventDefault();
       var myHeaders = new Headers();
       myHeaders.append("Cookie", "csrftoken=GgguGBOp7YuM79PBjdw8jazMIromyg4F");
-
-      // var urlencoded = new URLSearchParams();
-      // urlencoded.append("username", values.username);
-      // urlencoded.append("password", values.password);
-      // urlencoded.append("role", values.role);
 
       var requestOptions = {
          method: "GET",
@@ -48,8 +68,8 @@ function Login() {
             throw Error(response.status);
          })
          .then((result) => {
-            console.log(result);
-            // setUser(result);
+            // console.log(result);
+            setValues(result);
             navigate("/card");
          })
          .catch((error) => {
@@ -67,80 +87,112 @@ function Login() {
 
    return (
       <div>
-         {/* {fetchToken() ? (
-            <p>You are login</p>
-         ) : ( */}
-         <Container maxWidth="sm">
-            <Grid
-               container
-               spacing={2}
-               direction="column"
-               justifyContent="center"
-               style={{ minHeight: "100vh" }}
-            >
-               <Paper elevation={2} sx={{ padding: 5 }}>
-                  <form onSubmit={handleSubmit}>
-                     <Grid container direction="column" spacing={2}>
-                        <Grid item>
-                           <TextField
-                              type="text"
-                              fullWidth
-                              label="UserName"
-                              placeholder="UserName"
-                              variant="outlined"
-                              onChange={(e) =>
-                                 setValues({
-                                    ...values,
-                                    username: e.target.value,
-                                 })
-                              }
-                           />
-                        </Grid>
-
-                        <Grid item>
-                           <TextField
-                              type={values.showPass ? "text" : "password"}
-                              fullWidth
-                              label="Password"
-                              placeholder="Password"
-                              variant="outlined"
-                              onChange={(e) =>
-                                 setValues({
-                                    ...values,
-                                    password: e.target.value,
-                                 })
-                              }
-                              InputProps={{
-                                 endAdornment: (
-                                    <InputAdornment position="end">
-                                       <IconButton
-                                          onClick={handlePass}
-                                          aria-label="toggle password"
-                                          edge="end"
-                                       >
-                                          {values.showPass ? (
-                                             <VisibilityOffIcon />
-                                          ) : (
-                                             <VisibilityIcon />
-                                          )}
-                                       </IconButton>
-                                    </InputAdornment>
-                                 ),
-                              }}
-                           />
-                        </Grid>
-
-                        <Grid item>
-                           <Button type="submit" variant="contained" fullWidth>
-                              Sign In
-                           </Button>
-                        </Grid>
-                     </Grid>
-                  </form>
-               </Paper>
+         <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: "100vh" }}>
+               <CssBaseline />
+               <Grid
+                  item
+                  xs={false}
+                  sm={4}
+                  md={7}
+                  sx={{
+                     backgroundImage: "url(https://source.unsplash.com/random)",
+                     backgroundRepeat: "no-repeat",
+                     backgroundColor: (t) =>
+                        t.palette.mode === "light"
+                           ? t.palette.grey[50]
+                           : t.palette.grey[900],
+                     backgroundSize: "cover",
+                     backgroundPosition: "center",
+                  }}
+               />
+               <Grid
+                  item
+                  xs={12}
+                  sm={8}
+                  md={5}
+                  component={Paper}
+                  elevation={6}
+                  square
+               >
+                  <Box
+                     sx={{
+                        my: 8,
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                     }}
+                  >
+                     <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                     </Avatar>
+                     <Typography component="h1" variant="h5">
+                        Sign in to Englist Hub Website
+                     </Typography>
+                     <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 1 }}
+                     >
+                        <TextField
+                           type="text"
+                           fullWidth
+                           label="UserName"
+                           placeholder="UserName"
+                           variant="outlined"
+                           onChange={(e) =>
+                              setValues({
+                                 ...values,
+                                 username: e.target.value,
+                              })
+                           }
+                        />
+                        <TextField
+                           type={values.showPass ? "text" : "password"}
+                           fullWidth
+                           label="Password"
+                           placeholder="Password"
+                           variant="outlined"
+                           onChange={(e) =>
+                              setValues({
+                                 ...values,
+                                 password: e.target.value,
+                              })
+                           }
+                           InputProps={{
+                              endAdornment: (
+                                 <InputAdornment position="end">
+                                    <IconButton
+                                       onClick={handlePass}
+                                       aria-label="toggle password"
+                                       edge="end"
+                                    >
+                                       {values.showPass ? (
+                                          <VisibilityOffIcon />
+                                       ) : (
+                                          <VisibilityIcon />
+                                       )}
+                                    </IconButton>
+                                 </InputAdornment>
+                              ),
+                           }}
+                        />
+                        <Button
+                           type="submit"
+                           fullWidth
+                           variant="contained"
+                           sx={{ mt: 3, mb: 2 }}
+                        >
+                           Sign In
+                        </Button>
+                        <Copyright sx={{ mt: 5 }} />
+                     </Box>
+                  </Box>
+               </Grid>
             </Grid>
-         </Container>
-         {/* )} */}
+         </ThemeProvider>
       </div>
    );
 }
